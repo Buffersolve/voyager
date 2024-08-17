f0ca8ba73c8904a4edd18c7bc0039af08af6cb62
VolodymyrLA <volodymyrlabish@nerdzlab.com>
2024-08-17 18:58:22 +0300

    change java ver

--- buildSrc/src/main/kotlin/Setup.kt
+++ buildSrc/src/main/kotlin/Setup.kt
@@ -53,8 +53,8 @@ fun Project.setupModuleForAndroidxCompose(
         }
 
         compileOptions {
-            sourceCompatibility = JavaVersion.VERSION_1_8
-            targetCompatibility = JavaVersion.VERSION_1_8
+            sourceCompatibility = JavaVersion.VERSION_11
+            targetCompatibility = JavaVersion.VERSION_11
         }
 
         testOptions {
